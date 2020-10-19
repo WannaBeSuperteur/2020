1c40b314e85bf92aeed03d2add70e5cb7c0c6b3a
WannaBeSuperteur <32893014+WannaBeSuperteur@users.noreply.github.com>
2020-10-19 17:23:59 +0900

    modify: add comment for each formula

--- WPCN_UAV/2020_10_Multi_UAV_Enabled/formula.py
+++ WPCN_UAV/2020_10_Multi_UAV_Enabled/formula.py
@@ -7,10 +7,12 @@ def eucNorm(array):
     for i in range(len(array)): result += pow(array[i], 2)
     return math.sqrt(result)
 
+# the distance between UAV l and device k[l]
 # d[n][l][k[l]] = sqrt((x[n][l] - x[k[l]])^2 + (y[n][l] - y[k[l]])^2 + h[n][l]^2)
 def d_nlkl(n, l, k, x, y, h):
     return math.sqrt(pow(x[n][l]-x[k[l]], 2) + pow(y[n][l]-y[k[l]], 2) + pow(h[n][l], 2))
 
+# the collected energy of each IoT device k[l] at period T
 # E[k[l]] = Sum(i=1,L)(ng * alpha * T * a[0][i] * g[0][i][k[l]] * P^D), ... (6)
 #           where all l in L, kl in K
 def E_kl(L, ng, alpha, T, a, g, k, l, PD):
@@ -20,6 +22,7 @@ def E_kl(L, ng, alpha, T, a, g, k, l, PD):
 
     return result
 
+# available energy E_[n][k[l]] of device k[l] in n-th time slot
 # E_[n][k[l]] = E[k[l]] - Sum(j=1,n-1)(a[j][l][k[l]] * SN * PU[j][k[l]]) ... (7)
 def E_nkl(n, k, l, SN, PU, L, ng, alpha, T, a, g, PD):
     result = E_kl(L, ng, alpha, T, a, g, k, l, PD)
@@ -28,12 +31,14 @@ def E_nkl(n, k, l, SN, PU, L, ng, alpha, T, a, g, PD):
 
     return result
 
+# instantaneous throughput R_[n][k[l]] of IoT device k[l]
 # R_[n][k[l]] = B[k[l]]*log2(1 + r[n][k[l]]) ... (10)
 #               where r[n][k[l]] = P^U[n][k[l]]*g[n][l][k[l]] / (I[n][k[l]] + o^2)
 def R_nkl(B, k, l, n, PU, g, I_, o2):
     r = PU[n][k[l]] * g[n][l][k[l]] / (I_[n][k[l]] + o2)
     return B[k[l]] * math.log(1+r, 2)
 
+# the average throughput R[k[l]] of IoT device k[l] of the flight cycle T
 # R[k[l]] = (1/T) * Sum(n=1,N)(a[n][l][k[l]] * R_[n][k[l]]) ... (11)
 def R_kl(T, N, l, k, B, n, PU, g, I_, o2):
     result = 0
@@ -41,7 +46,7 @@ def R_kl(T, N, l, k, B, n, PU, g, I_, o2):
         result += a[n][l][k[l]] * R_nkl(B, k, l, n, PU, g, I_, o2)
     return result / T
 
-# q[l](t) = (x[l](t), y[l](t), h[l](t))
+# q[l](t) = (x[l](t), y[l](t), h[l](t)) - location of UAV[l] at time t
 # q[n][l] = [x[n][l], y[n][l], h[n][l]]
 def q_nl(x, y, h, n, l):
     return [x[n][l], y[n][l], h[n][l]]
