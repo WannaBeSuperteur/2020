2436314355062de2aed55d60a6c8f3c741e5ac36
WannaBeSuperteur <32893014+WannaBeSuperteur@users.noreply.github.com>
2020-10-30 16:11:18 +0900

    modify: add deep learning feature

--- AI/kaggle/2020_10_ConwayReverseGame/answer.py
+++ AI/kaggle/2020_10_ConwayReverseGame/answer.py
@@ -7,7 +7,7 @@ import deepLearning_main as DL
 
 if __name__ == '__main__':
     np.set_printoptions(edgeitems=30, linewidth=180)
-    verbose = True
+    verbose = False
 
     # ~_sub_0.txt : with delta 1
     # ~_sub_1.txt : with delta 2
@@ -17,12 +17,27 @@ if __name__ == '__main__':
 
     # save real training and test data to use deep learning, for each case delta=1,2,3,4 and 5
     for i in range(5):
+
+        # file names and configurations
+        trainIName = 'train_input_sub_' + str(i) + '.txt'
+        trainOName = 'train_output_sub_' + str(i) + '.txt'
+        testIName = 'test_input_sub_' + str(i) + '.txt'
+        testOName = 'test_output_sub_' + str(i) + '.txt' # file to make
+        testReport = 'test_report_sub_' + str(i) + '.txt' # file to make
+        validRate = 0.05
+        validReport = 'valid_report_sub_' + str(i) + '.txt' # file to make
+        modelConfig = 'model_sub_' + str(i) + '.txt' # file to make
+
+        deviceName = 'cpu:0'
+        epoch = 20
+
+        # load arrays
         train_id_list = RD.loadArray('train_id_sub_' + str(i) + '.txt')
-        trainI_array = RD.loadArray('train_input_sub_' + str(i) + '.txt')
-        trainO_array = RD.loadArray('train_output_sub_' + str(i) + '.txt')
+        trainI_array = RD.loadArray(trainIName)
+        trainO_array = RD.loadArray(trainOName)
 
         test_id_list = RD.loadArray('test_id_sub_' + str(i) + '.txt')
-        testI_array = RD.loadArray('test_input_sub_' + str(i) + '.txt')
+        testI_array = RD.loadArray(testIName)
 
         # print training and test array
         if verbose == True:
@@ -35,4 +50,5 @@ if __name__ == '__main__':
                 print(np.array(testI_array)[j].reshape(20, 20))
 
         # deep learning : test array -> training array
-        # DL.deepLearning(trainNames[i], 'output_delta' + str(i), testNames[i], 
+        DL.deepLearning(trainIName, trainOName, testIName, testOName, None, testReport,
+                        validRate, validReport, modelConfig, deviceName, epoch, verbose, 'model_sub_' + str(i))
