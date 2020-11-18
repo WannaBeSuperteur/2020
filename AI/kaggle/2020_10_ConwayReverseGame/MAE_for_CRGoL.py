d16346398bf5ac480ac1ca1e605d7dc43a3f0134
WannaBeSuperteur <32893014+WannaBeSuperteur@users.noreply.github.com>
2020-11-18 14:51:10 +0900

    modify: add portion of 0 and 1 in prediction

--- AI/kaggle/2020_10_ConwayReverseGame/MAE_for_CRGoL.py
+++ AI/kaggle/2020_10_ConwayReverseGame/MAE_for_CRGoL.py
@@ -12,7 +12,14 @@ def readValidReport(fn, thresholdList, n):
 
     # read each line and compute MAE
     MAE = [] # MAE values (for each threshold)
-    for i in range(len(thresholdList)): MAE.append(0.0)
+    zeros = [] # count of number 0
+    ones = [] # count of number 1
+    
+    for i in range(len(thresholdList)):
+        MAE.append(0.0)
+        zeros.append(0.0)
+        ones.append(0.0)
+        
     lines = len(fl)-9 # number of lines
     
     for i in range(lines):
@@ -31,18 +38,30 @@ def readValidReport(fn, thresholdList, n):
             for k in range(n):
 
                 # pred is considered as 1 -> MAE=0 if real=1, MAE=1 if real=0
-                if predSplit[k] > thresholdList[j]: MAE[j] += (1 - int(realSplit[k]))
+                if predSplit[k] > thresholdList[j]:
+                    MAE[j] += (1 - int(realSplit[k]))
+                    ones[j] += 1.0
 
                 # pred is considered as 0 -> MAE=0 if real=0, MAE=1 if real=1
-                else: MAE[j] += int(realSplit[k])
+                else:
+                    MAE[j] += int(realSplit[k])
+                    zeros[j] += 1.0
 
     # MAE <- MAE / (lines * n)
-    for i in range(len(thresholdList)): MAE[i] = MAE[i] / (lines * n)
+    # zeros,ones <- zeros,ones / (number of total count)
+    for i in range(len(thresholdList)):
+        MAE[i] = MAE[i] / (lines * n)
+
+        zeros_ = zeros[i]
+        ones_ = ones[i]
+        zeros[i] = zeros[i] / (zeros_ + ones_)
+        ones[i] = ones[i] / (zeros_ + ones_)
         
     # print MAE
     result = ''
     for i in range(len(thresholdList)):
-        result_ = 'MAE when threshold = ' + str(round(thresholdList[i], 6)) + '\tis ' + str(round(MAE[i], 6))
+        result_ = ('MAE when threshold = ' + str(round(thresholdList[i], 6)) + '\tis ' + str(round(MAE[i], 6))
+                   + '\t0 in pred = ' + str(round(zeros[i], 6)) + ',\t1 in pred = ' + str(round(ones[i], 6)))
         print(result_)
         result += result_ + '\n'
 
@@ -54,4 +73,8 @@ def readValidReport(fn, thresholdList, n):
 if __name__ == '__main__':
     thresholdList = []
     for i in range(1, 100): thresholdList.append(round(0.01*i, 6))
-    readValidReport('valid_report_sub_0.txt', thresholdList, 400)
+    readValidReport('valid_report_n_sub_0.txt', thresholdList, 1)
+    readValidReport('valid_report_n_sub_1.txt', thresholdList, 1)
+    readValidReport('valid_report_n_sub_2.txt', thresholdList, 1)
+    readValidReport('valid_report_n_sub_3.txt', thresholdList, 1)
+    readValidReport('valid_report_n_sub_4.txt', thresholdList, 1)
