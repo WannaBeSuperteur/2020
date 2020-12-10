8b1190054c39bfcc5fbdf3eee4564a6896112f67
WannaBeSuperteur <32893014+WannaBeSuperteur@users.noreply.github.com>
2020-12-10 17:52:11 +0900

    upload base code

---
+++ AI/kaggle/2020_11_ConwayReverseGame2020/MAE_for_CRGoL.py
@@ -0,0 +1,216 @@
+import math
+import numpy as np
+
+# file input and output
+# input  : validation report
+# output : validation report with weighted-average for each cell
+#          validation MAE with changing threshold
+
+# files read by this code
+# valid_report_n_sub_X.txt (open in readValidReport)
+
+# files written by this code
+# valid_report_n_sub_X_weightedAvg.txt (open in readValidReport)
+# valid_report_n_sub_X_MAE.txt (open in readValidReport)
+
+# board_i_y and board_i_x -> index of the data
+def xyToIndex(board_i_y, board_i_x, i, size):
+    i_start = int(i / (size*size)) * (size*size)
+    return i_start + board_i_y * size + board_i_x
+
+# get weighted prediction
+# n               : number of cells in test output array ( = rows * rows = len(weight))
+# i               : index
+# size            : board size (25 for this problem)
+# board_i_y       : y-axis value of index i of the board
+# board_i_x       : x-axis value of index i of the board
+# weight          : weight values
+# readValidReport : reading valid report (True or False)
+# fl              : file.readlines() (valid report : valid_report_n_sub_X.txt, or test output : test_output_n_sub_X.txt)
+def getWeightedPrediction(n, i, size, board_i_y, board_i_x, weight, readValidReport, fl):
+
+    # compute prediction using weighted average (weight: defined in array 'weight')
+    weightedPred = 0.0
+    sumWeight = 0.0
+
+    # number of rows in each output array (n = rows * rows board)
+    rows = int(math.sqrt(n)) # number of rows in test output
+    outputWS = int((rows-1)/2) # output window size
+    
+    y_start = board_i_y - outputWS
+    x_start = board_i_x - outputWS
+
+    refList = []
+    refVals = []
+    refWeights = []
+        
+    for j in range(max(0, board_i_y - outputWS), min(size, board_i_y + outputWS + 1)):
+        for k in range(max(0, board_i_x - outputWS), min(size, board_i_x + outputWS + 1)):
+
+            # read predicted output of this index
+            outputIndex = xyToIndex(j, k, i, size)
+
+            # find output values (= valid/test prediction)
+            if readValidReport == True: # valid prediction output
+                pred_ = fl[outputIndex].split('pred = [')[1].split(']')[0]
+                output = np.array(' '.join(pred_.split()).split(' ')).astype('float')
+            else: # test prediction output
+                
+                # result value array (with output size)
+                output = fl[outputIndex].split('\n')[0].split('\t')
+                output = output[:len(output)-1]
+                output = np.array(output).astype('float')
+
+            # update weighted prediction
+            refPosition = (j - y_start) * rows + (k - x_start) # position of the location width index (j, k) in array 'weight'
+             
+            weightedPred += weight[refPosition] * output[n - 1 - refPosition]
+            sumWeight += weight[refPosition]
+
+            refList.append(outputIndex)
+            refVals.append(round(output[n - 1 - refPosition], 6))
+            refWeights.append(weight[refPosition])
+
+    return (weightedPred / sumWeight, refList, refVals, refWeights)
+
+# available for BINARY (0 or 1) values only
+# fn            : file name
+# thresholdList : list for thresholds (above->1, below->0)
+# size          : input board size (width or height = 25)
+# n             : number of values in each array pred = [...] or real = [...]
+# use_n_sub     : n_sub mode (True or False)
+def readValidReport(fn, thresholdList, size, n, use_n_sub):
+
+    # read validation report
+    f = open(fn, 'r')
+    fl = f.readlines()
+    f.close()
+
+    # weight for each cell (left-top ~ right-bottom of output board)
+    weight = [1]
+
+    # read each line and compute MAE
+    MAE = [] # MAE values (for each threshold)
+    zeros = [] # count of number 0
+    ones = [] # count of number 1
+    weightedAvgResult = str(weight) + '\n' # to write valid_report_n_sub_X_weightedAvg.txt
+    
+    for i in range(len(thresholdList)):
+        MAE.append(0.0)
+        zeros.append(0.0)
+        ones.append(0.0)
+        
+    lines = len(fl)-9 # number of lines
+    
+    for i in range(lines):
+
+        # read predicted and real values
+        pred = fl[i].split('pred = [')[1].split(']')[0] # pred = [...]
+        real = fl[i].split('real = [')[1].split(']')[0] # real = [...]
+
+        # convert to float
+        predSplit = np.array(' '.join(pred.split()).split(' ')).astype('float')
+        realSplit = np.array(' '.join(real.split()).split(' ')).astype('float')
+
+        # check if length of predSplit and realSplit are the same
+        try:
+            assert(len(predSplit) == len(realSplit))
+        except:
+            print('ERROR: Length of prediction (pred = [...]) and real (real = [...]) are not the same.')
+            return
+
+        # <<< use n_sub mode >>>
+        if use_n_sub == True:
+
+            # center of output vector
+            center = int((n-1)/2)
+            
+            # index of the line (indexed by i) for the board
+            board_i = i % (size * size)
+            board_i_y = int(board_i / size) # y-axis value of index i of the board
+            board_i_x = board_i % size # x-axis value of index i of the board
+
+            # find weighted prediction
+            (weightedPred, refList, refVals, refWeights) = getWeightedPrediction(n, i, size, board_i_y, board_i_x, weight, True, fl)
+
+            # divide by sum of the weights and use it as prediction
+            weightedAvgResult += ('[' + str(i) + '] refList=' + str(refList) + ' refVals=' + str(refVals) + ' refWeights=' + str(refWeights)
+                                  + ' weightedPred=' + str(round(weightedPred, 6)) + '\n')
+
+            # compute MAE for each threshold
+            for j in range(len(thresholdList)):
+                
+                # pred is considered as 1 -> MAE=0 if real=1, MAE=1 if real=0
+                if weightedPred > thresholdList[j]:
+                    MAE[j] += (1 - int(realSplit[center]))
+                    ones[j] += 1.0
+
+                # pred is considered as 0 -> MAE=0 if real=0, MAE=1 if real=1
+                else:
+                    MAE[j] += int(realSplit[center])
+                    zeros[j] += 1.0
+
+        # <<< use normal mode >>>
+        else:
+
+            # compute MAE for each threshold
+            for j in range(len(thresholdList)):
+                for k in range(len(predSplit)):
+                
+                    # pred is considered as 1 -> MAE=0 if real=1, MAE=1 if real=0
+                    if predSplit[k] > thresholdList[j]:
+                        MAE[j] += (1 - int(realSplit[k]))
+                        ones[j] += 1.0
+
+                    # pred is considered as 0 -> MAE=0 if real=0, MAE=1 if real=1
+                    else:
+                        MAE[j] += int(realSplit[k])
+                        zeros[j] += 1.0
+
+    # MAE <- MAE / (lines * n)
+    # zeros,ones <- zeros,ones / (number of total count)
+    for i in range(len(thresholdList)):
+
+        # for MAE, divide by the number of rows
+        MAE[i] /= lines
+
+        # divide by length of output vector for normal mode
+        if use_n_sub == False: MAE[i] /= len(predSplit)
+
+        # for zeros and ones, divide to make the sum 1.0
+        zeros_ = zeros[i]
+        ones_ = ones[i]
+        zeros[i] = zeros[i] / (zeros_ + ones_)
+        ones[i] = ones[i] / (zeros_ + ones_)
+        
+    # print MAE
+    result = '' # to write valid_report_n_sub_X_MAE.txt
+    for i in range(len(thresholdList)):
+        result_ = ('MAE when threshold =\t' + str(round(thresholdList[i], 6)) + '\tis\t' + str(round(MAE[i], 6))
+                   + '\t0 in pred =\t' + str(round(zeros[i], 6)) + '\t1 in pred =\t' + str(round(ones[i], 6)))
+        print(result_)
+        result += result_ + '\n'
+
+    # write to file
+    f = open(fn[:len(fn)-4] + '_MAE.txt', 'w')
+    f.write(result)
+    f.close()
+
+    # write weighted average only when use_n_sub is True
+    if use_n_sub == True:
+        f = open(fn[:len(fn)-4] + '_weightedAvg.txt', 'w')
+        f.write(weightedAvgResult)
+        f.close()
+
+if __name__ == '__main__':
+    thresholdList = [] # list of thresholds to use
+    size = 25 # the number of rows/columns in each input data
+    outputSize = 1 # the number of rows/columns in each output data
+    outputLength = outputSize * outputSize # length of output vector
+    
+    for i in range(-20, 221): thresholdList.append(round(0.005*i, 6))
+    readValidReport('valid_report_n_sub_0.txt', thresholdList, size, outputLength)
+    readValidReport('valid_report_n_sub_1.txt', thresholdList, size, outputLength)
+    readValidReport('valid_report_n_sub_2.txt', thresholdList, size, outputLength)
+    readValidReport('valid_report_n_sub_3.txt', thresholdList, size, outputLength)
+    readValidReport('valid_report_n_sub_4.txt', thresholdList, size, outputLength)
---
+++ AI/kaggle/2020_11_ConwayReverseGame2020/answer.py
@@ -0,0 +1,148 @@
+import sys
+import numpy as np
+sys.path.insert(0, '../../../../AI_BASE')
+
+import readData as RD
+import deepLearning_main as DL
+import MAE_for_CRGoL as MAE
+
+# file input and output
+# input  : below (files read by this code)
+# output : deep learning result (according to validation rate, n_sub or not, use_n_sub_for_test or not)
+#          if validation_rate > 0, write validation report (valid_report_n_sub_X.txt)
+#                                  validation report with threshold (valid_report_n_sub_X_MAE.txt)
+
+# files read by this code
+#                                            train_id_sub_X.txt       (loadArray)
+#          [normal]                          train_input_sub_X.txt    (loadArray)
+#          [normal]                          train_output_sub_X.txt   (loadArray)
+#          [n_sub]                           train_input_n_sub_X.txt  (loadArray)
+#          [n_sub]                           train_output_n_sub_X.txt (loadArray)
+# [test]                                     test_id_sub_X.txt        (loadArray)
+# [test]   [n_sub]  [use_n_sub_for_test]     test_input_n_sub_X.txt   (loadArray)
+# [test]   [n_sub]  [not use_n_sub_for_test] test_input_sub_X.txt     (loadArray)
+# [test]   [normal]                          test_input_sub_X.txt     (loadArray)
+#                                            config.txt               (deepLearning)
+#          [normal]                          model_sub_X.txt          (deepLearning)
+#          [n_sub]                           model_n_sub_X.txt        (deepLearning)
+
+if __name__ == '__main__':
+    np.set_printoptions(edgeitems=30, linewidth=180)
+    verbose = False
+    use_n_sub = True # using 'train_input/output_n_sub_X.txt' as training input/output data
+    use_n_sub_for_test = False # using n_sub for test input data ( -> test_input_n_sub_X.txt exists)
+
+    # ~_sub_0.txt : with delta 1
+    # ~_sub_1.txt : with delta 2
+    # ~_sub_2.txt : with delta 3
+    # ~_sub_3.txt : with delta 4
+    # ~_sub_4.txt : with delta 5
+
+    # threshold list to find MAE
+    thresholdList = []
+    for i in range(-20, 221): thresholdList.append(round(0.005*i, 6))
+    
+    n = [5, 7, 9, 11, 13]
+    epochs = [5, 5, 5, 5, 5]
+    size = 25 # the number of rows/columns in each input data
+    outputSize = 1 # the number of rows/columns in each output data (only for n-sub mode)
+
+    # save real training and test data to use deep learning, for each case delta=1,2,3,4 and 5
+    for i in range(5):
+
+        validRate = 0.05 # validation rate for deep learning
+        deviceName = 'cpu:0'
+        epoch = epochs[i]
+
+        # file names and configurations
+        if use_n_sub == True: # use n-sub mode
+            trainIName = 'train_input_n_sub_' + str(i) + '.txt'
+            trainOName = 'train_output_n_sub_' + str(i) + '.txt'
+            if use_n_sub_for_test == True: testIName = 'test_input_n_sub_' + str(i) + '.txt'
+            else: testIName = 'test_input_sub_' + str(i) + '.txt'
+            testOName = 'test_output_n_sub_' + str(i) + '.txt' # file to make
+            testReport = 'test_report_n_sub_' + str(i) + '.txt' # file to make
+            validReport = 'valid_report_n_sub_' + str(i) + '.txt' # file to make
+            modelConfig = 'model_n_sub_' + str(i) + '.txt' # file to make
+            modelName = 'model_n_sub_' + str(i) # model name
+
+        else: # do not use n-sub mode ( -> use normal mode)
+            trainIName = 'train_input_sub_' + str(i) + '.txt'
+            trainOName = 'train_output_sub_' + str(i) + '.txt'
+            testIName = 'test_input_sub_' + str(i) + '.txt'
+            testOName = 'test_output_sub_' + str(i) + '.txt' # file to make
+            testReport = 'test_report_sub_' + str(i) + '.txt' # file to make
+            validReport = 'valid_report_sub_' + str(i) + '.txt' # file to make
+            modelConfig = 'model_sub_' + str(i) + '.txt' # file to make
+            modelName = 'model_sub_' + str(i) # model name
+
+        # load arrays (no difference between normal and n-sub mode)
+        train_id_list = RD.loadArray('train_id_sub_' + str(i) + '.txt')
+        trainI_array = RD.loadArray(trainIName)
+        trainO_array = RD.loadArray(trainOName)
+
+        if validRate == 0.0: # for test mode
+            test_id_list = RD.loadArray('test_id_sub_' + str(i) + '.txt')
+            testI_array = RD.loadArray(testIName)
+
+        # print training and test array (consider n-sub mode)
+        if verbose == True:
+            for j in range(5):
+                trainI_ = np.array(trainI_array)[j]
+                trainO_ = np.array(trainO_array)[j]
+                if validRate == 0.0: testI_ = np.array(testI_array)[j] # for test mode
+
+                # use math.sqrt because trainI, trainO and testI are always n*n square
+                print('\ntrainI_array = stop - delta=' + str(i+1) + ', index=' + str(j))
+                print(trainI_.reshape(int(math.sqrt(len(trainI_))), int(math.sqrt(len(trainI_)))))
+                print('\ntrainO_array = start - delta=' + str(i+1) + ', index=' + str(j))
+                print(trainO_.reshape(int(math.sqrt(len(trainO_))), int(math.sqrt(len(trainO_)))))
+                if validRate == 0.0:
+                    print('\ntestI_array = stop - delta=' + str(i+1) + ', index=' + str(j))
+                    print(testI_.reshape(int(math.sqrt(len(testI_))), int(math.sqrt(len(testI_)))))
+
+        # deep learning : test array -> training array
+
+        #### validation mode ####
+        # test input is not used -> just use training data file to make model
+        # regardless of existance of test input file
+        if validRate > 0:
+            DL.deepLearning(trainIName, trainOName, None, None, None, testReport,
+                            validRate, validReport, modelConfig, deviceName, epoch, verbose, modelName)
+
+        #### use_n_sub == False or use_n_sub_for_test == True ####
+        # training input, training output and test input are all exist -> just use this data file to make model
+        elif use_n_sub == False or use_n_sub_for_test == True:
+            DL.deepLearning(trainIName, trainOName, testIName, testOName, None, testReport,
+                            validRate, validReport, modelConfig, deviceName, epoch, verbose, modelName)
+
+        #### use_n_sub == True and use_n_sub_for_test == False ####
+        # training input, training ouput and model exist, but test input does not exist
+        # -> read and reshape test data first as read.makeData
+        else:
+
+            # read and reshape test data from each file
+            testInputData = [] # test input data for testing, whose size = data index (test input #, size, size) * each data (n, n)
+            ws = int((n[i] - 1)/2)
+
+            delta = i + 1
+            testInput = RD.loadArray('test_input_sub_' + str(delta-1) + '.txt')
+            testInput = np.array(testInput).astype('float')
+
+            # for each test input
+            for j in range(len(testInput)):
+                thisReshaped = np.pad(np.array(testInput[j]).reshape(size, size), ((ws, ws), (ws, ws)), 'wrap')
+
+                # save test data into array testInputData
+                for k in range(ws, size+ws):
+                    for l in range(ws, size+ws):
+                        testInputData.append(list(thisReshaped[k-ws:k+ws+1, l-ws:l+ws+1].reshape(n[i]*n[i])))
+
+            # do deep learning
+            DL.deepLearning(trainIName, trainOName, testInputData, testOName, None, testReport,
+                            validRate, validReport, modelConfig, deviceName, epoch, verbose, modelName)
+
+        # print MAE
+        if validRate > 0:
+            if use_n_sub == True: MAE.readValidReport(validReport, thresholdList, size, outputSize*outputSize, True) # use n-sub mode
+            else: MAE.readValidReport(validReport, thresholdList, size, size*size, False) # do not use n-sub mode ( -> use normal mode)
