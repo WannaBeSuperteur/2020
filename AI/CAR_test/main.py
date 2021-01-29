4d37f6ba9c8d50c6c7f2c180395628166efe64c9
WannaBeSuperteur <32893014+WannaBeSuperteur@users.noreply.github.com>
2021-01-29 18:34:10 +0900

    add codes and config txts for CAR DATASET test

---
+++ AI/CAR_test/main.py
@@ -0,0 +1,167 @@
+import helper
+import sys
+sys.path.insert(0, '../../AI_BASE')
+
+import numpy as np
+import readData as RD
+import deepLearning_main as DL
+
+# function to read image from csv file
+# colRange: [a, None] - retrieve columns with indices a, ..., (last column)
+#           [a, b]    - retrieve columns with indices a, ..., b-1
+# rowRange: [a, None] - retrieve rows    with indices a, ..., (last row)
+#           [a, b]    - retrieve rows    with indices a, ..., b-1
+def readCSV(fn, colRange, rowRange, delimiter=','):
+    csv = RD.loadArray(fn, delimiter)
+
+    # colRange start and end, rowRange start and end
+    cs = colRange[0]
+    ce = colRange[1]
+    rs = rowRange[0]
+    re = rowRange[1]
+
+    # colRange: [a, None]
+    if ce == None:
+
+        # rowRange: [a, None]
+        if re == None: return np.array(csv)[rs:, cs:]
+
+        # rowRange: [a, b]
+        else: return np.array(csv)[rs:re, cs:]
+
+    # colRange: [a, b]
+    else:
+
+        # rowRange: [a, None]
+        if re == None: return np.array(csv)[rs:, cs:ce]
+
+        # rowRange: [a, b]
+        else: return np.array(csv)[rs:re, cs:ce]
+
+# one-hot
+def one_hot(array):
+    result = []
+    
+    for i in range(len(array)):
+        result.append([0, 0])
+        result[i][int(array[i][0])] = 1
+
+    return result
+
+# convert to numeric value (/255) and save
+def convertToNumeric():
+
+    # number of train rows and test rows
+    trainRows = 1486
+    testRows = 382
+
+    # check if converted data file exists
+    try:
+        _ = open('car_train_input.txt', 'r')
+        _.close()
+        _ = open('car_train_output.txt', 'r')
+        _.close()
+        _ = open('car_test_input.txt', 'r')
+        _.close()
+        _ = open('car_test_output.txt', 'r')
+        _.close()
+        return
+    except:
+        pass
+    
+    # TRAINING DATA
+    # read and print col=1, ... and row=1, ... of the CSV file
+    print('reading training input...')
+    train_input = readCSV('train.csv', [1, None], [1, trainRows+1])
+    
+    print('reading training output...')
+    train_output = readCSV('train.csv', [0, 1], [1, trainRows+1])
+
+    # TEST DATA
+    print('reading test input...')
+    test_input = readCSV('test.csv', [1, None], [1, testRows+1])
+    
+    print('reading test output...')
+    test_output = readCSV('test.csv', [0, 1], [1, testRows+1])
+
+    # make training data numeric (inverting the color)
+    for i in range(trainRows):
+        if i % 25 == 0: print(i)
+        
+        for j in range(len(train_input[0])):
+            train_input[i][j] = 1.0 - int(train_input[i][j]) / 255.0
+
+    # make test data numeric (inverting the color)
+    for i in range(testRows):
+        if i % 25 == 0: print(i)
+        
+        for j in range(len(test_input[0])):
+            test_input[i][j] = 1.0 - int(test_input[i][j]) / 255.0
+
+    # make output one-hot
+    train_output = list(train_output)
+    test_output = list(test_output)
+
+    train_output = one_hot(train_output)
+    test_output = one_hot(test_output)
+
+    # save
+    print('saving training input...')
+    RD.saveArray('car_train_input.txt', train_input, splitter='\t', saveSize=50)
+    
+    print('saving training output...')
+    RD.saveArray('car_train_output.txt', train_output, splitter='\t', saveSize=50)
+    
+    print('saving test input...')
+    RD.saveArray('car_test_input.txt', test_input, splitter='\t', saveSize=50)
+    
+    print('saving test output...')
+    RD.saveArray('car_test_output.txt', test_output, splitter='\t', saveSize=50)
+
+# read training and test data
+def readcarData():
+    print('reading car data...')
+    
+    # TRAINING DATA
+    # read and print col=1, ... and row=1, ... of the CSV file
+    train_input = np.array(RD.loadArray('car_train_input.txt'))
+    train_output = np.array(RD.loadArray('car_train_output.txt'))
+    print(train_input)
+    print(train_output)
+
+    # TEST DATA
+    test_input = np.array(RD.loadArray('car_test_input.txt'))
+    test_output = np.array(RD.loadArray('car_test_output.txt'))
+    print(test_input)
+    print(test_output)
+
+    return (train_input, train_output, test_input, test_output)
+
+# main
+if __name__ == '__main__':
+
+    # meta info
+    TRI = 'car_train_input.txt'
+    TRO = 'car_train_output.txt'
+    TEI = 'car_test_input.txt'
+    TEO = 'car_test_predict.txt'
+
+    TE_real = 'car_test_output.txt'
+    TE_report = 'car_test_report.txt'
+    VAL_rate = 0.0
+    VAL_report = 'car_val_report.txt'
+    modelConfig = 'car_model_config.txt'
+
+    # preprocess data
+    convertToNumeric()
+
+    # user data
+    deviceName = input('device name (for example, cpu:0 or gpu:0)')
+    epoch = int(input('epoch'))
+    printed = int(input('printed? (0 -> do not print)'))
+
+    # training and test
+    # 'config.txt' is used for configuration
+    DL.deepLearning(TRI, TRO, TEI, TEO,
+                    TE_real, TE_report, VAL_rate, VAL_report, modelConfig,
+                    deviceName, epoch, printed, 'model')
---
+++ AI/CAR_test/my_extract.py
@@ -0,0 +1,147 @@
+from PIL import Image
+import os
+import sys
+sys.path.insert(0, '../../AI_BASE')
+import readData as RD
+import random
+
+# extract images from 'myData' directory
+def extractImages():
+
+    # try to create 'images' directory
+    try:
+        os.mkdir('images')
+    except:
+        pass
+    
+    # extract images
+    for r, _, f in os.walk('myData'):
+
+        count = 0
+
+        # for each file in /myData
+        for file in f:
+
+            # label of image: 0, 1, ..., 42
+            label = r.split('\\')[1]
+            imgFile = os.path.join(r, file)
+
+            # print the process
+            if count % 100 == 0: print(label, count)
+            count += 1
+
+            # resize into 32*32 images
+            # change image name: *.png to *.jpg
+            # save image at 'images' folder
+            pat = os.path.join(r, file)
+            
+            with Image.open(pat) as im:
+                if im.size!=(192, 128):
+                    im=im.resize((192, 128), Image.LANCZOS)
+                im.save('images/' + label + ') ' + file.replace(".png",".jpg"))
+
+# OFEC: output for each class
+#       in the form of [class0output, class1output, ..., class42output]
+def makeCsv():
+
+    # write CSV file: for example, (10x10 images with label 0, 1, ..., or 7)
+    
+    # label 0,0 0,1 0,2 0,3 ... 0,9 1,0 ... 1,9 2,0 ... 9,9
+    # 4     0   0   0   0   ... 0   0   ... 255 255 ... 0
+    # 1     0   255 204 51  ... 0   51  ... 255 0   ... 0
+    # 5     0   0   102 153 ... 0   0   ... 0   0   ... 135
+    # 0     0   0   0   0   ... 65  173 ... 0   0   ... 0
+    # 1     0   0   0   0   ... 102 200 ... 12  0   ... 0
+    # ...   ... ... ... ... ... ... ... ... ... ... ... ...
+    # 7     0   0   0   102 ... 102 225 ... 195 0   ... 0
+
+    # initialize array
+    csvArray = []
+
+    # write first row
+    firstRow = ['label']
+    
+    imgWidth = 192
+    imgHeight = 128
+    for i in range(imgHeight):
+        for j in range(imgWidth):
+            firstRow.append(str(i) + '_' + str(j))
+
+    csvArray.append(firstRow)
+
+    # convert each image into a row for the CSV file
+    files = os.listdir('images')
+    count = 0
+    
+    for file in files:
+
+        # for count
+        if count % 25 == 0: print(count, len(files))
+        count += 1
+
+        # open each image file
+        img = Image.open('images/' + file)
+        pixel = img.load()
+
+        # add each pixel
+        OFEC_list = {'train_car':100, 'train_bus':101, 'test_car':102, 'test_bus':103}
+        label = OFEC_list[file.split(')')[0]]
+        thisRow = [label]
+        
+        for i in range(imgWidth):
+            for j in range(imgHeight):
+                thisRow.append(int(sum(pixel[i, j])/3))
+
+        csvArray.append(thisRow)
+
+    # save into file
+    RD.saveArray('train_test.csv', csvArray, splitter=',', saveSize=50)
+
+# extract training data and test data
+def extractTrainAndTest():
+
+    # load the csv file including all the data (except for the first row)
+    csvArray = RD.loadArray('train_test.csv', splitter=',')
+    csvArray = csvArray[1:]
+    numOfRows = len(csvArray)
+
+    # write first row
+    firstRow = ['label']
+    
+    imgWidth = 192
+    imgHeight = 128
+    for i in range(imgHeight):
+        for j in range(imgWidth):
+            firstRow.append(str(i) + '_' + str(j))
+
+    # designate training data and test data
+    trainingData = [firstRow]
+    testData = [firstRow]
+
+    for i in range(numOfRows):
+
+        # train or test
+        if int(csvArray[i][0]) >= 102: train = False
+        else: train = True
+
+        # car: 0, bus: 1
+        csvArray[i][0] = str(int(csvArray[i][0]) % 2)
+
+        # append to training/test data
+        if train == True: trainingData.append(csvArray[i])
+        else: testData.append(csvArray[i])
+
+    # save file (trainingData and testData)
+    RD.saveArray('train.csv', trainingData, splitter=',', saveSize=1000)
+    RD.saveArray('test.csv', testData, splitter=',', saveSize=1000)
+
+if __name__ == '__main__':
+
+    # extract images from 'myData' directory
+    #extractImages()
+
+    # make CSV file from the extracted images
+    #makeCsv()
+
+    # extract training data and test data
+    extractTrainAndTest()
