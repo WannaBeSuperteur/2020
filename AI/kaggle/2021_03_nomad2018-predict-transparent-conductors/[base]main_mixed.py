0420fb5a27c1fde1b3d25ce6dda60dc0d253006d
WannaBeSuperteur <32893014+WannaBeSuperteur@users.noreply.github.com>
2021-03-09 12:30:42 +0900

    modify: completed CatBoost option

--- AI/kaggle/2021_03_nomad2018-predict-transparent-conductors/[base]main_mixed.py
+++ AI/kaggle/2021_03_nomad2018-predict-transparent-conductors/[base]main_mixed.py
@@ -81,6 +81,51 @@ def create_dataframe(TRI_array, TRO_array, TEI_array,
 
         return (TRI_df, TRO_df, VAI_df, VAO_df)
 
+### WRITE RESULT (COMMON) ###
+def writeResult(predict_tv, tv_output, VAL_rate, num, modelName):
+    
+    # write result
+    result = []
+    for i in range(len(predict_tv)):
+        result.append([predict_tv[i]])
+
+    if VAL_rate > 0:
+        RD.saveArray(modelName + '_val_result_' + str(num) + '.txt', result, '\t', 500)
+    else:
+        RD.saveArray(modelName + '_test_result_' + str(num) + '.txt', result, '\t', 500)
+
+    # validation mode -> compute RMSLE error
+    # saved results are still NORMALIZED values
+    if VAL_rate > 0:
+
+        # convert prediction
+        tv_output_ = []
+        predict_tv_ = []
+
+        # for tv_output
+        for i in range(len(tv_output[0])):
+
+            if num == 0: # formation_energy_ev_natom
+                tv_output_.append(float(tv_output[0][i]) * 0.104078 + 0.187614)
+
+            else: # bandgap_energy_ev
+                tv_output_.append(float(tv_output[0][i]) * 1.006635 + 2.077205)
+
+        # for predict_tv
+        for i in range(len(predict_tv)):
+
+            if num == 0: # formation_energy_ev_natom
+                predict_tv_.append(float(predict_tv[i]) * 0.104078 + 0.187614)
+
+            else: # bandgap_energy_ev
+                predict_tv_.append(float(predict_tv[i]) * 1.006635 + 2.077205)
+
+        # compute RMSLE and return
+        print('\n\n ====[ ' + modelName + ' / valid=True ]====')
+        return rmsle(tv_output_, predict_tv_)
+
+    return 0
+
 ################################
 ##                            ##
 ##    model 00 : lightGBM     ##
@@ -133,50 +178,10 @@ def model_00_lightGBM(TRI_array, TRO_array, TEI_array, TEO, TE_report, VAL_rate,
         model = lgb.train(params, train_ds, 5000, train_ds, verbose_eval=50, early_stopping_rounds=200)
 
     # predict
-    predict_train = model.predict(train_input)
     predict_tv = model.predict(tv_input)
 
-    # write result
-    result = []
-    for i in range(len(predict_tv)):
-        result.append([predict_tv[i]])
-
-    if VAL_rate > 0:
-        RD.saveArray('m00_lightGBM_val_result_' + str(num) + '.txt', result, '\t', 500)
-    else:
-        RD.saveArray('m00_lightGBM_test_result_' + str(num) + '.txt', result, '\t', 500)
-
-    # validation mode -> compute RMSLE error
-    # saved results are still NORMALIZED values
-    if VAL_rate > 0:
-
-        # convert prediction
-        tv_output_ = []
-        predict_tv_ = []
-
-        # for tv_output
-        for i in range(len(tv_output[0])):
-
-            if num == 0: # formation_energy_ev_natom
-                tv_output_.append(float(tv_output[0][i]) * 0.104078 + 0.187614)
-
-            else: # bandgap_energy_ev
-                tv_output_.append(float(tv_output[0][i]) * 1.006635 + 2.077205)
-
-        # for predict_tv
-        for i in range(len(predict_tv)):
-
-            if num == 0: # formation_energy_ev_natom
-                predict_tv_.append(float(predict_tv[i]) * 0.104078 + 0.187614)
-
-            else: # bandgap_energy_ev
-                predict_tv_.append(float(predict_tv[i]) * 1.006635 + 2.077205)
-
-        # compute RMSLE and return
-        print('\n\n ====[ lightGBM / valid=True ]====')
-        return rmsle(tv_output_, predict_tv_)
-
-    return 0
+    # write result and return RMSLE error
+    return writeResult(predict_tv, tv_output, VAL_rate, num, 'm00_lightGBM')
 
 ################################
 ##                            ##
@@ -204,27 +209,15 @@ def model_01_CatBoost(TRI_array, TRO_array, TEI_array, TEO, TE_report, VAL_rate,
 
     # create model
     if VAL_rate > 0:
-        model.fit(train_input, train_output, eval_set=(test_input, test_output), use_best_model=True, verbose=False)
+        model.fit(train_input, train_output, eval_set=(tv_input, tv_output), use_best_model=True, verbose=False)
     else:
         model.fit(train_input, train_output, eval_set=None, use_best_model=True, verbose=False)
 
     # predict
-    predict_train = model.predict(train_input)
     predict_tv = model.predict(tv_input)
 
-    # write result
-
-    #### ING ####
-
-    # validation mode -> compute RMSLE error
-    # saved results are still NORMALIZED values
-    if VAL_rate > 0:
-
-        #### ING ####
-        
-        pass
-
-    return 0
+    # write result and return RMSLE error
+    return writeResult(predict_tv, tv_output, VAL_rate, num, 'm01_CatBoost')
 
 ################################
 ##                            ##
@@ -291,9 +284,10 @@ if __name__ == '__main__':
             testValResult_01 = RD.loadArray('m01_CatBoost_test_result_' + str(num) + '.txt')
 
         # write final result
-        finalResult = []
+        finalResult_00 = []
+        finalResult_01 = []
             
-        for i in range(len(testValResult)):
+        for i in range(len(testValResult_00)):
                 
             if num == 0: # formation_energy_ev_natom
                 finalResult_00.append([float(testValResult_00[i][0]) * 0.104078 + 0.187614])
