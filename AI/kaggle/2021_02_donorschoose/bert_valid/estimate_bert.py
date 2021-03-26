64baa7ec0231ecf81868178c0fa481151715d9cd
WannaBeSuperteur <32893014+WannaBeSuperteur@users.noreply.github.com>
2021-03-26 16:43:58 +0900

    modify: use length of text columns

--- AI/kaggle/2021_02_donorschoose/bert_valid/estimate_bert.py
+++ AI/kaggle/2021_02_donorschoose/bert_valid/estimate_bert.py
@@ -40,6 +40,9 @@ class TEXT_MODEL(tf.keras.Model):
         #        find the loss function for ROC-AUC (other than mean-squared-error)
         #        to find more valuable variables from codes
         #        [V] callbacks : val_loss (for early stopping) and lr_reduced
+        #        make l_den deeper, into l_den0, l_den1, ...
+        #        [V] use the length of each text (title, essays, ...)
+        #        split project_subject_categories and project_subject_subcategories
 
         # i0_tp   : teacher_prefix                (   6 unique items)
         # i1_ss   : school_state                  (  51 unique items)
@@ -50,12 +53,13 @@ class TEXT_MODEL(tf.keras.Model):
         # i6_pgc  : project_grade_category        (   4 unique items)
         # i7_psc  : project_subject_categories    (  51 unique items)
         # i8_pss  : project_subject_subcategories ( 407 unique items)
-        # i9_cont :                                   1
+        # i9_len  :                                   6
+        # i10_ts  :                                   1
         #
-        # total   : i0_tp, i1_ss, ..., i5_cont    ( 565 unique columns)
+        # total   : i0_tp, i1_ss, ..., i10_ts     ( 571 unique columns)
         # text    : tokenized by BERT             ( 818        columns)
         #
-        #                                         (1383        columns)
+        #                                         (1389        columns)
 
         # constants
         self.u00, self.u01, self.u02, self.u03, self.u04 = 6, 51, 2, 12, 7
@@ -67,51 +71,58 @@ class TEXT_MODEL(tf.keras.Model):
         self.d00, self.d01, self.d02, self.d03, self.d04 = 4, 4, 4, 4, 4
         self.d05, self.d06, self.d07, self.d08           = 4, 4, 4, 4
         
-        self.d09, self.dL = 4, 4
+        self.d09, self.d09_final, self.d10, self.dL = 32, 4, 4, 4
 
         # flatten layer
         self.flat = tf.keras.layers.Flatten()
 
         # for info 0, 1, 2, 3, 4, 5, 6, 7, 8 and i9_cont
-        self.emb00 = tf.keras.layers.Embedding(self.u00, self.e00, trainable=True)
-        self.emb01 = tf.keras.layers.Embedding(self.u01, self.e01, trainable=True)
-        self.emb02 = tf.keras.layers.Embedding(self.u02, self.e02, trainable=True)
-        self.emb03 = tf.keras.layers.Embedding(self.u03, self.e03, trainable=True)
-        self.emb04 = tf.keras.layers.Embedding(self.u04, self.e04, trainable=True)
-        self.emb05 = tf.keras.layers.Embedding(self.u05, self.e05, trainable=True)
-        self.emb06 = tf.keras.layers.Embedding(self.u06, self.e06, trainable=True)
-        self.emb07 = tf.keras.layers.Embedding(self.u07, self.e07, trainable=True)
-        self.emb08 = tf.keras.layers.Embedding(self.u08, self.e08, trainable=True)
+        L2 = tf.keras.regularizers.l2(0.001)
         
-        self.den00 = tf.keras.layers.Dense(units=self.d00, activation='relu')
-        self.den01 = tf.keras.layers.Dense(units=self.d01, activation='relu')
-        self.den02 = tf.keras.layers.Dense(units=self.d02, activation='relu')
-        self.den03 = tf.keras.layers.Dense(units=self.d03, activation='relu')
-        self.den04 = tf.keras.layers.Dense(units=self.d04, activation='relu')
-        self.den05 = tf.keras.layers.Dense(units=self.d05, activation='relu')
-        self.den06 = tf.keras.layers.Dense(units=self.d06, activation='relu')
-        self.den07 = tf.keras.layers.Dense(units=self.d07, activation='relu')
-        self.den08 = tf.keras.layers.Dense(units=self.d08, activation='relu')
-        self.den09 = tf.keras.layers.Dense(units=self.d09, activation='relu')
+        self.emb00 = tf.keras.layers.Embedding(self.u00, self.e00, trainable=True, name='emb00')
+        self.emb01 = tf.keras.layers.Embedding(self.u01, self.e01, trainable=True, name='emb01')
+        self.emb02 = tf.keras.layers.Embedding(self.u02, self.e02, trainable=True, name='emb02')
+        self.emb03 = tf.keras.layers.Embedding(self.u03, self.e03, trainable=True, name='emb03')
+        self.emb04 = tf.keras.layers.Embedding(self.u04, self.e04, trainable=True, name='emb04')
+        self.emb05 = tf.keras.layers.Embedding(self.u05, self.e05, trainable=True, name='emb05')
+        self.emb06 = tf.keras.layers.Embedding(self.u06, self.e06, trainable=True, name='emb06')
+        self.emb07 = tf.keras.layers.Embedding(self.u07, self.e07, trainable=True, name='emb07')
+        self.emb08 = tf.keras.layers.Embedding(self.u08, self.e08, trainable=True, name='emb08')
+        
+        self.den00 = tf.keras.layers.Dense(units=self.d00, activation='relu', kernel_regularizer=L2, name='den00')
+        self.den01 = tf.keras.layers.Dense(units=self.d01, activation='relu', kernel_regularizer=L2, name='den01')
+        self.den02 = tf.keras.layers.Dense(units=self.d02, activation='relu', kernel_regularizer=L2, name='den02')
+        self.den03 = tf.keras.layers.Dense(units=self.d03, activation='relu', kernel_regularizer=L2, name='den03')
+        self.den04 = tf.keras.layers.Dense(units=self.d04, activation='relu', kernel_regularizer=L2, name='den04')
+        self.den05 = tf.keras.layers.Dense(units=self.d05, activation='relu', kernel_regularizer=L2, name='den05')
+        self.den06 = tf.keras.layers.Dense(units=self.d06, activation='relu', kernel_regularizer=L2, name='den06')
+        self.den07 = tf.keras.layers.Dense(units=self.d07, activation='relu', kernel_regularizer=L2, name='den07')
+        self.den08 = tf.keras.layers.Dense(units=self.d08, activation='relu', kernel_regularizer=L2, name='den08')
+        
+        self.den09_0 = tf.keras.layers.Dense(units=self.d09, activation='relu', kernel_regularizer=L2, name='CONT_den09_0')
+        self.den09_1 = tf.keras.layers.Dense(units=self.d09, activation='relu', kernel_regularizer=L2, name='CONT_den09_1')
+        self.den09_2 = tf.keras.layers.Dense(units=self.d09, activation='relu', kernel_regularizer=L2, name='CONT_den09_2')
+        self.den09_final = tf.keras.layers.Dense(units=self.d09_final, activation='relu', kernel_regularizer=L2, name='CONT_den09_final')
+        self.den10 = tf.keras.layers.Dense(units=self.d10, activation='relu', kernel_regularizer=L2, name='CONT_den10')
 
         # for BERT-tokenized text
-        self.embedding = tf.keras.layers.Embedding(vocabulary_size, embedding_dimensions, trainable=True)
-        self.cnn_layer1 = tf.keras.layers.Conv1D(filters=cnn_filters, kernel_size=2, padding='valid', activation='relu')
-        self.cnn_layer2 = tf.keras.layers.Conv1D(filters=cnn_filters, kernel_size=3, padding='valid', activation='relu')
-        self.cnn_layer3 = tf.keras.layers.Conv1D(filters=cnn_filters, kernel_size=4, padding='valid', activation='relu')
-        self.pool = tf.keras.layers.GlobalMaxPool1D()
-        self.denL = tf.keras.layers.Dense(units=self.dL, activation='relu')
+        self.embedding = tf.keras.layers.Embedding(vocabulary_size, embedding_dimensions, trainable=True, name='BERT_embedding')
+        self.cnn_layer1 = tf.keras.layers.Conv1D(filters=cnn_filters, kernel_size=2, padding='valid', activation='relu', name='BERT_cnn1')
+        self.cnn_layer2 = tf.keras.layers.Conv1D(filters=cnn_filters, kernel_size=3, padding='valid', activation='relu', name='BERT_cnn2')
+        self.cnn_layer3 = tf.keras.layers.Conv1D(filters=cnn_filters, kernel_size=4, padding='valid', activation='relu', name='BERT_cnn3')
+        self.pool = tf.keras.layers.GlobalMaxPool1D(name='BERT_pooling')
+        self.denL = tf.keras.layers.Dense(units=self.dL, activation='relu', kernel_regularizer=L2, name='BERT_dense')
 
         # for concatenated layer and output
-        self.dense = tf.keras.layers.Dense(units=dnn_units, activation='relu')
-        self.dropout = tf.keras.layers.Dropout(rate=dropout_rate)
-        self.last_dense = tf.keras.layers.Dense(units=1, activation='sigmoid') # output layer
+        self.dense = tf.keras.layers.Dense(units=dnn_units, activation='relu', kernel_regularizer=L2, name='FINAL_dense0')
+        self.dropout = tf.keras.layers.Dropout(rate=dropout_rate, name='dropout')
+        self.last_dense = tf.keras.layers.Dense(units=1, activation='sigmoid', name='output') # output layer
 
     def call(self, inputs, training):
 
         # split input
-        i0_tp, i1_ss, i2_d_y, i3_d_m, i4_d_d, i5_d_h, i6_pgc, i7_psc, i8_pss, i9_cont, inputs_text =\
-        tf.split(inputs, [self.u00, self.u01, self.u02, self.u03, self.u04, self.u05, self.u06, self.u07, self.u08, 1, 818], 1)
+        i0_tp, i1_ss, i2_d_y, i3_d_m, i4_d_d, i5_d_h, i6_pgc, i7_psc, i8_pss, i9_len, i10_ts, inputs_text =\
+        tf.split(inputs, [self.u00, self.u01, self.u02, self.u03, self.u04, self.u05, self.u06, self.u07, self.u08, 6, 1, 818], 1)
 
         inputs_text = tf.cast(inputs_text, tf.int32)
 
@@ -119,40 +130,56 @@ class TEXT_MODEL(tf.keras.Model):
         i0_emb = self.emb00(i0_tp)
         i0_emb = self.flat(i0_emb)
         i0_den = self.den00(i0_emb)
+        i0_drop = self.dropout(i0_den, training)
         
         i1_emb = self.emb01(i1_ss)
         i1_emb = self.flat(i1_emb)
         i1_den = self.den01(i1_emb)
+        i1_drop = self.dropout(i1_den, training)
         
         i2_emb = self.emb02(i2_d_y)
         i2_emb = self.flat(i2_emb)
         i2_den = self.den02(i2_emb)
+        i2_drop = self.dropout(i2_den, training)
         
         i3_emb = self.emb03(i3_d_m)
         i3_emb = self.flat(i3_emb)
         i3_den = self.den03(i3_emb)
+        i3_drop = self.dropout(i3_den, training)
         
         i4_emb = self.emb04(i4_d_d)
         i4_emb = self.flat(i4_emb)
         i4_den = self.den04(i4_emb)
+        i4_drop = self.dropout(i4_den, training)
 
         i5_emb = self.emb05(i5_d_h)
         i5_emb = self.flat(i5_emb)
         i5_den = self.den05(i5_emb)
+        i5_drop = self.dropout(i5_den, training)
         
         i6_emb = self.emb06(i6_pgc)
         i6_emb = self.flat(i6_emb)
         i6_den = self.den06(i6_emb)
+        i6_drop = self.dropout(i6_den, training)
         
         i7_emb = self.emb07(i7_psc)
         i7_emb = self.flat(i7_emb)
         i7_den = self.den07(i7_emb)
+        i7_drop = self.dropout(i7_den, training)
         
         i8_emb = self.emb08(i8_pss)
         i8_emb = self.flat(i8_emb)
         i8_den = self.den08(i8_emb)
+        i8_drop = self.dropout(i8_den, training)
         
-        i9_den = self.den09(i9_cont)
+        i9_den0 = self.den09_0(i9_len)
+        i9_den1 = self.den09_1(i9_den0)
+        i9_den2 = self.den09_2(i9_den1)
+        i9_denFinal = self.den09_final(i9_den2)
+        i9_drop = self.dropout(i9_denFinal, training)
+
+        i10_den = self.den10(i10_ts)
+        i10_drop = self.dropout(i10_den, training)
 
         # BERT-tokenized text
         l = self.embedding(inputs_text)
@@ -166,9 +193,12 @@ class TEXT_MODEL(tf.keras.Model):
         
         l_concat = tf.concat([l_1, l_2, l_3], axis=-1)
         l_den = self.denL(l_concat)
+        l_drop = self.dropout(l_den, training)
 
         # concatenate embedded info and concatenated BERT-tokenized text
-        concatenated = tf.concat([i0_den, i1_den, i2_den, i3_den, i4_den, i5_den, i6_den, i7_den, i8_den, i9_den, l_den], axis=-1)
+        concatenated = tf.concat([i0_drop, i1_drop, i2_drop, i3_drop, i4_drop, i5_drop,
+                                  i6_drop, i7_drop, i8_drop, i9_drop, i10_drop, l_drop], axis=-1)
+        
         concatenated = self.dense(concatenated)
         concatenated = self.dropout(concatenated, training)
         model_output = self.last_dense(concatenated)
@@ -245,14 +275,14 @@ if __name__ == '__main__':
 
     # define configuration
     train_max_rows = 50000 # 9999999 (no limit)
-    valid_max_rows = 10000 # 9999999 (no limit)
+    valid_max_rows = 9999999
     print_interval = 400
     batch_size = 32
 
     embedding_dim = 200
     cnn_filters = 50
     dnn_units = 256
-    dropout_rate = 0.2
+    dropout_rate = 0.5
 
     loss = 'mse'
     opti = optimizers.Adam(0.0005, decay=1e-6)
@@ -294,7 +324,7 @@ if __name__ == '__main__':
     train_info = []
     valid_info = []
 
-    option = [-1, -1, 2, 2, 7, 2, 2, 2, -1, -1, -1, -1, -1, -1, 1, -1]
+    option = [-1, -1, 2, 2, 7, 2, 2, 2, 5, 5, 5, 5, 5, 5, 1, -1]
     title = ['id', 'teacher_id', 'teacher_prefix', 'school_state',
              'project_submitted_datetime', 'project_grade_category',
              'project_subject_categories', 'project_subject_subcategories',
@@ -404,8 +434,8 @@ if __name__ == '__main__':
         print(np.array(train_approved[:100]))
 
         # callback list for training
-        early = tf.keras.callbacks.EarlyStopping(monitor="val_loss", mode="min", patience=2)
-        lr_reduced = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=2, verbose=1, epsilon=0.0001, mode='min')
+        early = tf.keras.callbacks.EarlyStopping(monitor="val_loss", mode="min", patience=3)
+        lr_reduced = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=1, verbose=1, epsilon=0.0001, mode='min')
 
         ### TRAIN THE MODEL ###
         text_models[i].fit(train_data, train_approved, validation_split=0.4, callbacks=[early, lr_reduced], epochs=epochs)
