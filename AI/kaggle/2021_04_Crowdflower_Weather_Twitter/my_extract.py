decb2341f7a21cfdaf3853b46a1f6912e8a9d400
WannaBeSuperteur <32893014+WannaBeSuperteur@users.noreply.github.com>
2021-04-23 17:23:42 +0900

    modify: bugfix and use symbol for states

--- AI/kaggle/2021_04_Crowdflower_Weather_Twitter/my_extract.py
+++ AI/kaggle/2021_04_Crowdflower_Weather_Twitter/my_extract.py
@@ -6,8 +6,34 @@ import pandas as pd
 import re
 
 # write input data
-def writeInput(array_input, state_list):
+def writeInput(array_input, state_list, modify_state_list):
 
+    # transform state_list: ['alabama', 'alaska', ...]
+    #                  into [['alabama', 'AL'], ['alaska', 'AK'], ...]
+    # because 'NYC' includes 'NY', no need to find 'NYC' for New York
+    if modify_state_list == True:
+        state_set = {'alabama':'AL', 'alaska':'AK', 'arizona':'AZ',
+                     'arkansas':'AR', 'california':'CA', 'colorado':'CO',
+                     'connecticut':'CT', 'delaware':'DE', 'florida':'FL',
+                     'georgia':'GA', 'hawaii':'HI', 'idaho':'ID',
+                     'illinois':'IL', 'indiana':'IN', 'iowa':'IA',
+                     'kansas':'KS', 'kentucky':'KY', 'louisiana':'LA',
+                     'maine':'ME', 'maryland':'MD', 'massachusetts':'MA',
+                     'michigan':'MI', 'minnesota':'MN', 'mississippi':'MS',
+                     'missouri':'MO', 'montana':'MT', 'nebraska':'NE',
+                     'nevada':'NV', 'new hampshire':'NH', 'new jersey':'NJ',
+                     'new mexico':'NM', 'new york':'NY', 'north carolina':'NC',
+                     'north dakota':'ND', 'ohio':'OH', 'oklahoma':'OK',
+                     'oregon':'OR', 'pennsylvania':'PA', 'rhode island':'RI',
+                     'south carolina':'SC', 'south dakota':'SD', 'tennessee':'TN',
+                     'texas':'TX', 'utah':'UT', 'vermont':'VT',
+                     'virginia':'VA', 'washington':'WA', 'west virginia':'WV',
+                     'wisconsin':'WI', 'wyoming':'WY', 'district of columbia':'DC'}
+        
+        for i in range(len(state_list)):
+            state_list[i] = [state_list[i], state_set[state_list[i]]]
+
+    # write input
     final_input = []
 
     for i in range(len(array_input)):
@@ -16,10 +42,23 @@ def writeInput(array_input, state_list):
         # tweet (remove alphabet and space only)
         thisRow.append(re.sub('[^a-zA-Z ]', '', array_input[i][0]))
 
-        # state
+        # state or location
         for j in range(len(state_list)):
-            if array_input[i][1] == state_list[j]:
+
+            # state name exist
+            if state_list[j][0] == array_input[i][1]:
                 thisRow.append(1)
+
+            # state symbol (AL, AK, ...) exist
+            elif array_input[i][2] != None:
+                try:
+                    if state_list[j][1] in array_input[i][2]:
+                        thisRow.append(1)
+                    else:
+                        thisRow.append(falseVal)
+                except:
+                    thisRow.append(falseVal)
+                    
             else:
                 thisRow.append(falseVal)
 
@@ -34,20 +73,19 @@ if __name__ == '__main__':
 
     # create train input and output
     train_data = np.array(pd.read_csv('train.csv'))
-    train_input = train_data[1:, 1:4]
-    train_output = train_data[1:, 4:]
+    train_input = train_data[:, 1:4]
+    train_output = train_data[:, 4:]
 
     test_data = np.array(pd.read_csv('test.csv'))
-    test_input = test_data[1:, 1:4]
+    test_input = test_data[:, 1:4]
 
     # one-hot for state
     state_list = list(set(train_data[:, 2]))
-    print(state_list)
 
     # write final train_input and test_input
     # test output is as original
-    final_train_input = writeInput(train_input, state_list)
-    final_test_input = writeInput(test_input, state_list)
+    final_train_input = writeInput(train_input, state_list, True)
+    final_test_input = writeInput(test_input, state_list, False)
 
     # save training and test data
     RD.saveArray('train_input.txt', final_train_input, '\t', 500)
