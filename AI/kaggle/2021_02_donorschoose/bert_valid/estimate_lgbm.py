7c8f52fc7906bed3d0191c325319730d02e7376e
WannaBeSuperteur <32893014+WannaBeSuperteur@users.noreply.github.com>
2021-05-25 17:17:13 +0900

    add: lightGBM solution code

---
+++ AI/kaggle/2021_02_donorschoose/bert_valid/estimate_lgbm.py
@@ -0,0 +1,203 @@
+# train_train.csv, train_test.csv -> bert_train_result.txt, bert_test_result.txt
+# additional need: bert_max_lengths_train.txt
+
+# ref: https://insightcampus.co.kr/insightcommunity/?mod=document&uid=12854
+#      https://stackabuse.com/text-classification-with-bert-tokenizer-and-tf-2-0-in-python/
+#      https://www.kaggle.com/linwenxing/glove-vector-lstm
+
+import sys
+sys.path.insert(0, '../../AI_BASE')
+import my_extract as ME
+
+import math
+import numpy as np
+import readData as RD
+import deepLearning_main as DL
+import pandas as pd
+import random
+
+import tensorflow as tf
+
+import warnings
+warnings.filterwarnings('ignore')
+warnings.filterwarnings('always')
+
+import lightgbm as lgb
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.metrics import mean_squared_error, r2_score, roc_auc_score
+
+# create dataframe
+def create_dataframe(TRI_array, TRO_array, TEI_array, TEO_array=None):
+
+    TRI_df = pd.DataFrame(np.array(TRI_array).astype(float))
+    TRO_df = pd.DataFrame(np.array(TRO_array).astype(float))
+    TEI_df = pd.DataFrame(np.array(TEI_array).astype(float))
+
+    if TEO_array != None:
+        TEO_df = pd.DataFrame(np.array(TEO_array).astype(float))
+        return (TRI_df, TRO_df, TEI_df, TEO_df)
+    else:
+        return (TRI_df, TRO_df, TEI_df)
+
+# use lightGBM
+def lightGBM(TRI_array, TRO_array, TEI_array, TEO_array, count, valid):
+
+    # create Pandas DataFrame and convert to lightgbm dataset
+    # tv_input  : test / validation input
+    # tv_output : test / validation output
+    if valid == True:
+        (train_input, train_output, tv_input, tv_output) = create_dataframe(TRI_array, TRO_array, TEI_array, TEO_array)
+        train_ds = lgb.Dataset(train_input, label=train_output)
+        valid_ds = lgb.Dataset(tv_input, label=tv_output)
+
+    else:
+        (train_input, train_output, tv_input) = create_dataframe(TRI_array, TRO_array, TEI_array)
+        train_ds = lgb.Dataset(train_input, label=train_output)
+
+    # set parameters
+    # refer to https://www.kaggle.com/hiro5299834/tps-apr-2021-pseudo-labeling-voting-ensemble (0.81722)
+    params = {'metric': 'binary_logloss',
+              'objective': 'binary',
+              'random_state': 2021 + count, # SEED = 2021...
+              'learning_rate': 0.01,
+              'min_child_samples': 150,
+              'reg_alpha': 3e-5,
+              'reg_lambda': 9e-2,
+              'num_leaves': 20,
+              'max_depth': 16,
+              'colsample_bytree': 0.8,
+              'subsample': 0.8,
+              'subsample_freq': 2,
+              'max_bin': 240}
+
+    # create model
+    if valid == True:
+        model = lgb.train(params, train_ds, 2000, valid_ds, verbose_eval=20, early_stopping_rounds=200)
+    else:
+        model = lgb.train(params, train_ds, 2000, train_ds, verbose_eval=20, early_stopping_rounds=200)
+
+    # predict
+    predict_tv = model.predict(tv_input)
+    predictions = len(predict_tv)
+
+    if valid == True:
+        RD.saveArray('lightGBM_tv_valid_' + str(count) + '.txt', np.array([predict_tv]).T)
+    else:
+        RD.saveArray('lightGBM_tv_predict_' + str(count) + '.txt', np.array([predict_tv]).T)
+
+# designate rows to valid
+def designateValidRows(rows, validRate):
+    
+    isValidRow = []
+    for i in range(rows): isValidRow.append(False)
+    valid_count = 0
+
+    while valid_count < rows * validRate:
+        row = random.randint(0, rows-1)
+            
+        if isValidRow[row] == False:
+            isValidRow[row] = True
+            valid_count += 1
+
+    return isValidRow
+
+# main function
+def mainFunc(times):
+
+    # define configuration
+    trainFile = 'train.csv'
+    testFile = 'test.csv'
+    
+    trainExtractedFile = 'train_extracted_lgbm.txt'
+    testExtractedFile = 'test_extracted_lgbm.txt'
+    
+    trainTrainInputFile = 'train_train_input_lgbm.txt'
+    trainTrainOutputFile = 'train_train_output_lgbm.txt'
+    trainValidInputFile = 'train_valid_input_lgbm.txt'
+    trainValidOutputFile = 'train_valid_output_lgbm.txt'
+    
+    valid = True
+    validRate = 0.1
+
+    # extract data from training dataset
+    train_info = []
+    test_info = []
+
+    option = [-1, -1, 2, 2, 7, 2, 8, 8, 5, 5, 5, 5, 5, 5, 1, -1]
+    title = ['id', 'teacher_id', 'teacher_prefix', 'school_state',
+             'project_submitted_datetime', 'project_grade_category',
+             'project_subject_categories', 'project_subject_subcategories',
+             'project_title', 'project_essay_1', 'project_essay_2', 'project_essay_3', 'project_essay_4',
+             'project_resource_summary', 'teacher_number_of_previously_posted_projects', 'project_is_approved']
+
+    wordCount = ME.getWordCount(option, trainFile)
+
+    # load training output data
+    train_data = np.array(pd.read_csv(trainFile, dtype={11:str, 12:str}))
+    train_output = train_data[:, 15:16].astype(float)
+
+    # load data, if failed, save data
+    try:
+        train_input = RD.loadArray(trainExtractedFile, '\t')
+        test_input = RD.loadArray(testExtractedFile, '\t')
+        
+    except:
+        (train_input, _, onehot) = ME.extract(trainFile, option, title, wordCount, None, [2016, 2017])
+        (test_input, _, _) = ME.extract(testFile, option, title, wordCount, onehot, [2016, 2017])
+
+        RD.saveArray(trainExtractedFile, train_input, '\t', 500)
+        RD.saveArray(testExtractedFile, test_input, '\t', 500)
+
+    # separate valid dataset if valid==True
+    if valid == True:
+
+        rows = len(train_input)
+        
+        train_train_input = []
+        train_train_output = []
+        train_valid_input = []
+        train_valid_output = []
+
+        # designate rows to valid
+        isValidRow = designateValidRows(rows, validRate)
+        
+        # prepare train and valid dataset
+        for i in range(len(train_input)):  
+            if isValidRow[i]:
+                train_valid_input.append(train_input[i])
+                train_valid_output.append(train_output[i])
+            else:
+                train_train_input.append(train_input[i])
+                train_train_output.append(train_output[i])
+
+        # save files
+        RD.saveArray(trainTrainInputFile, train_train_input, '\t', 500)
+        RD.saveArray(trainTrainOutputFile, train_train_output, '\t', 500)
+        RD.saveArray(trainValidInputFile, train_valid_input, '\t', 500)
+        RD.saveArray(trainValidOutputFile, train_valid_output, '\t', 500)
+
+    # print dataset info
+    print('\ntrain_input:')
+    print(np.shape(train_input))
+    print(np.array(train_input))
+
+    print('\ntrain_output:')
+    print(np.shape(train_output))
+    print(np.array(train_output))
+
+    print('\ntest_input:')
+    print(np.shape(test_input))
+    print(np.array(test_input))
+
+    # predict using lightGBM
+    for count in range(times):
+        if valid == True:
+            lightGBM(train_train_input, train_train_output,
+                     train_valid_input, train_valid_output, count, True)
+        else:
+            lightGBM(train_input, train_output, test_input, None, count, False)
+
+if __name__ == '__main__':
+
+    # repeat 4 times
+    mainFunc(4)
--- AI/kaggle/2021_02_donorschoose/bert_valid/estimate_rocauc.py
+++ AI/kaggle/2021_02_donorschoose/bert_valid/estimate_rocauc.py
@@ -1,57 +1,36 @@
 import math
 import numpy as np
 import sys
-sys.path.insert(0, '../../../AI_BASE')
+sys.path.insert(0, '../../AI_BASE')
 import readData as RD
+
+import warnings
+warnings.filterwarnings('ignore')
+warnings.filterwarnings('always')
+
 from sklearn.metrics import roc_auc_score
-
-# convert value
-def convert(value, arr):
-
-    if value == 1.0: return 1.0
-    if value == 0.0: return 0.0
-
-    arrLen = len(arr) - 1
-
-    # when value=0.73, arr=[0.0, 0.5, 0.7, 0.85, 0.95, 1.0], arrLen=5
-    # -> loc   = value * arrLen = 0.73 * 5 = 3.65
-    loc = value * arrLen
-    
-    # -> left  = arr[int(3.65)]   = 0.85
-    # -> right = arr[int(3.65)+1] = 0.95
-    # -> rate  = 3.65 % 1.0       = 0.65
-    left = arr[int(loc)]
-    right = arr[int(loc) + 1]
-    rate = loc % 1.0
-
-    # to return: 0.85 + (0.95 - 0.85) * 0.65 = 0.915
-    result = left + (right - left) * rate
-    return result
 
 if __name__ == '__main__':
 
-    outputFileName = 'bert_valid_result.txt'
-    saveFileName = 'bert_valid_result_converted.txt'
-    arr = [0.0, 0.01, 0.03, 0.06, 0.1, 0.18, 0.32, 0.5, 0.68, 0.82, 0.9, 0.94, 0.97, 0.99, 1.0]
+    predictionFileNameFormat = 'lightGBM_tv_valid_'
+    answerFileName = 'train_valid_output_lgbm.txt'
+    times = 4
 
-    # read output data file
-    outputData = RD.loadArray(outputFileName)
+    # get prediction and answer
+    for i in range(times):
+        predictionFileName = predictionFileNameFormat + str(i) + '.txt'
 
-    for i in range(len(outputData)):
-        for j in range(len(outputData[i])):
-            outputData[i][j] = convert(float(outputData[i][j]), arr)
+        if i == 0:
+            prediction = np.array(RD.loadArray(predictionFileName)).astype(float).T[0]
+        else:
+            prediction += np.array(RD.loadArray(predictionFileName)).astype(float).T[0]
 
-    # save result
-    RD.saveArray(saveFileName, outputData, '\t', 500)
+    prediction /= times
 
-    # print ROC-AUC score
-    converted = np.array(RD.loadArray(saveFileName)).astype(float)
-    valid_answer = np.array(RD.loadArray('bert_valid_rightAnswer.txt')).astype(float)
+    answer = np.array(RD.loadArray(answerFileName)).astype(float).T[0]
 
-    pred_array = converted[:, 2]
-    real_array = valid_answer[:, 0]
+    # print prediction, answer and ROC-AUC score
+    print(prediction)
+    print(answer)
 
-    print(pred_array)
-    print(real_array)
-
-    print(round(roc_auc_score(real_array, pred_array), 4))
+    print(round(roc_auc_score(answer, prediction), 4))
