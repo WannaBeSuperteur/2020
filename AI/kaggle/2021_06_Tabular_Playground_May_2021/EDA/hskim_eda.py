62aa20770aaafee374a23bfcdc1b9d8c4780efb6
Hong Sik Kim <32893014+WannaBeSuperteur@users.noreply.github.com>
2021-06-23 15:52:04 +0900

    add: EDA of feature value distribution

--- AI/kaggle/2021_06_Tabular_Playground_May_2021/EDA/hskim_eda.py
+++ AI/kaggle/2021_06_Tabular_Playground_May_2021/EDA/hskim_eda.py
@@ -5,10 +5,31 @@ warnings.filterwarnings('always')
 
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import pandas as pd
 import numpy as np
 import math
 import csv
+
+# get distribution array (the number of items from min to max)
+# eg: [0, 1, 3, 1, 2, 7, 2] -> [[0,1], [1,2], [2,2], [3,1], [4,0], [5,0], [6,0], [7,1]]
+#     because min=0, max=7, count of 0,1,2,3,4,5,6,7 = 1,2,2,1,0,0,0,1
+def getDistribution(arr):
+
+    #minVal = int(min(arr))
+    #maxVal = int(max(arr))
+    minVal = -8 # use global minimum
+    maxVal = 66 # use global maximum
+
+    count_arr = np.zeros((maxVal - minVal + 1, 2))
+
+    for i in range(len(count_arr)):
+        count_arr[i][0] = minVal + i
+
+    for i in range(len(arr)):
+        count_arr[arr[i] - minVal][1] += 1
+
+    return count_arr
 
 if __name__ == '__main__':
 
@@ -75,7 +96,7 @@ if __name__ == '__main__':
             q3s[i][j] = q3 # quarter 3 (75%)
             maxs[i][j] = max_
 
-    # print FEATURE-TARGET INFO ARRAYS
+    # visualize FEATURE-TARGET INFO ARRAYS
     info0 = [averages, stddevs]
     info1 = [mins, q1s, q2s, q3s, maxs]
 
@@ -90,7 +111,7 @@ if __name__ == '__main__':
                              annot_kws={"size":8})
             count += 1
 
-        # show images
+        # save and show images
         if info == info0:
             plt.title('average and standard deviation for each class(4) for each feature(50)' + ' '*150, fontsize=10)
             plt.savefig('hskim_eda_avgAndStd.png')
@@ -101,5 +122,64 @@ if __name__ == '__main__':
 
     # FEATURE DISTRIBUTION arrays
     # print training feature distribution for each target value for each training feature
+    for i in range(50):
+        featureArr = np.array(trainData.loc[:, 'feature_' + str(i)])
+        featureMin = min(featureArr)
+        featureMax = max(featureArr)
 
-    # add distribution into the picture
+    # layout of visualization result
+    fig, axn = plt.subplots(features, numClass, sharex=True, sharey=True)
+    fig.set_size_inches(18, 150)
+
+    # list of FEATURE DISTRIBUTION for each class for each training feature
+    distribs = []
+
+    # extract cases where target='Class_1', ..., 'Class_4'
+    # for each training feature
+    for i in range(features):
+        featName = 'feature_' + str(i)
+        
+        class1 = trainData[trainData['target'] == 'Class_1'].loc[:, featName]
+        class2 = trainData[trainData['target'] == 'Class_2'].loc[:, featName]
+        class3 = trainData[trainData['target'] == 'Class_3'].loc[:, featName]
+        class4 = trainData[trainData['target'] == 'Class_4'].loc[:, featName]
+        classes = [class1, class2, class3, class4]
+
+        # find distribution for class 1, 2, 3 and 4
+        for j in range(numClass):
+
+            # get distribution array of each class
+            thisClass = np.array(classes[j])
+            thisClassDistrib = getDistribution(thisClass).astype(int)
+
+            # print
+            print('\n << feature ' + str(i) + ' class ' + str(j+1) + ' >>')
+            print(thisClassDistrib[:, 0].T)
+            print(thisClassDistrib[:, 1].T)
+
+            # append to list of FEATURE DISTRIBUTION
+            distribDf = pd.DataFrame(thisClassDistrib)
+            distribDf.columns = ['feature value', 'count (feat ' + str(i) + ')']
+            
+            distribs.append(distribDf)
+
+    # visualize the array
+    count = 0
+    for ax in axn.flat:
+        classNo = count % 4 + 1
+        featureNo = count // 4
+        print('drawing distribution chart for feature ' + str(featureNo) + ' class ' + str(classNo) + '...')
+        
+        distrib = distribs[count]
+        bp = sns.barplot(x='feature value', y='count (feat ' + str(featureNo) + ')', data=distrib, ax=ax, color='b')
+
+        # axis setting
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(5))
+        ax.yaxis.set_major_locator(ticker.MultipleLocator(10000))
+        
+        count += 1
+
+    # save and show images 
+    plt.title('distribution for each class(4) for each feature(50)' + ' '*200, fontsize=10)
+    plt.savefig('hskim_eda_distribution.png')
+    plt.show()
