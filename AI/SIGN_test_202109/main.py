acaf54b160bc9d66c1397fc21fc8aab466cb6dc9
Hong Sik Kim <32893014+WannaBeSuperteur@users.noreply.github.com>
2021-09-28 10:54:01 +0900

    modify: add evaluation and model saving

--- AI/SIGN_test_202109/main.py
+++ AI/SIGN_test_202109/main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
+from sklearn.metrics import mean_squared_error
 
 # define model (the number of input units not needed)
 def defineModel(learningRate=0.001):
@@ -44,6 +45,8 @@ if __name__ == '__main__':
     # load data
     train_data = np.array(pd.read_csv(train_file, index_col=0))
     test_data = np.array(pd.read_csv(test_file, index_col=0))
+    train_rows = len(train_data)
+    test_rows = len(test_data)
 
     print(np.shape(train_data))
     print(np.array(train_data))
@@ -67,25 +70,62 @@ if __name__ == '__main__':
     test_output = np.reshape(test_output, (test_output.shape[0], test_output.shape[2]))
 
     # print
+    print('train_data:')
     print(np.shape(train_data))
     print(np.array(train_data))
+
+    print('\ntest_data:')
     print(np.shape(test_data))
     print(np.array(test_data))
-    
+
+    print('\ntrain_input:')
     print(np.shape(train_input))
     print(np.array(train_input))
+
+    print('\ntrain_output:')
     print(np.shape(train_output))
     print(np.array(train_output))
+
+    print('\ntest_input:')
     print(np.shape(test_input))
     print(np.array(test_input))
+
+    print('\ntest_output:')
     print(np.shape(test_output))
     print(np.array(test_output))
 
+    pd.DataFrame(test_output).to_csv('signTest_groundTruth.csv')
+
     # model training
-    model = defineModel()
-    model.fit(train_input, train_output, epochs=3, validation_split=0.1)
+    try:
+        model = tf.keras.models.load_model('signTest_model')
+    except:
+        model = defineModel()
+        model.fit(train_input, train_output, epochs=10, validation_split=0.1)
+        model.save('signTest_model')
     model.summary()
 
     prediction = model.predict(test_input)
     prediction = pd.DataFrame(prediction)
     prediction.to_csv('signTest_prediction.csv')
+
+    # compare test groundTruth and test prediction
+    test_output_to_compare = np.array(test_output)
+    prediction_to_compare = np.array(prediction)
+
+    # compute mean-square error and accuracy
+    MSE = 0.0
+    accurateRows = 0
+
+    for i in range(test_rows):
+        ground_truth = test_output_to_compare[i]
+        pred = prediction_to_compare[i]
+        
+        MSE += mean_squared_error(ground_truth, pred)
+        if np.argmax(ground_truth) == np.argmax(pred): accurateRows += 1
+
+    MSE /= test_rows
+    accurateRows /= test_rows
+    
+    print('MSE      = ' + str(MSE))
+    print('accuracy = ' + str(accurateRows))
