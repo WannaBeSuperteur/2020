3005f8bb2a56db0d33fc228e5a87b55f03c1771c
Hong Sik Kim <32893014+WannaBeSuperteur@users.noreply.github.com>
2022-06-10 23:43:46 +0900

    add: save genetic-test trajectory as image

--- WPCN_UAV/2021_11_Multi_UAV/newGeneticTest.py
+++ WPCN_UAV/2021_11_Multi_UAV/newGeneticTest.py
@@ -1,5 +1,37 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import throughputTest_NewGenetic as T_NG
+
+# save trajectory as graph
+def saveTrajectory(width, height, initialLocUAV, locsUAV, deviceList):
+
+    N = len(locsUAV)
+
+    plt.clf()
+    plt.suptitle('trajectory result')
+    plt.axis([-1, width+1, -1, height+1])
+
+    # for each device
+    for i in range(len(deviceList)):
+        plt.scatter(deviceList[i][0], deviceList[i][1], s=25, marker='o', c='orange')
+
+    # for each location of UAV
+    for t in range(N):
+        if t == 0:
+            plt.scatter(initialLocUAV[0], initialLocUAV[1], s=25, marker='x', c='black')
+            plt.scatter(locsUAV[0][0], locsUAV[0][1], s=25, marker='x', c='forestgreen')
+            x = [initialLocUAV[0], locsUAV[0][0]]
+            y = [initialLocUAV[1], locsUAV[0][1]]
+            
+        else:
+            plt.scatter(locsUAV[t][0], locsUAV[t][1], s=25, marker='x', c='forestgreen')
+            x = [locsUAV[t-1][0], locsUAV[t][0]]
+            y = [locsUAV[t-1][1], locsUAV[t][1]]
+            
+        plt.plot(x, y, linewidth=0.75, c='forestgreen')
+
+    # save the figure
+    plt.savefig('newGeneticTest_trajectory.png')
 
 if __name__ == '__main__':
 
@@ -26,8 +58,8 @@ if __name__ == '__main__':
     
     deviceList = [[10, 20], [27, 20], [44, 20], [27, 39], [44, 39], [10, 58], [27, 58]]
 
-    # average of x : 27.00 (-> 28.00), y : 36.29, h : 15.00 (as same as the setting)
-    initialLocUAV = [28.0, 36.29, 15.0]
+    # average of x : 27.00 (-> 20.22), y : 36.29 (-> 20.23), h : 15.00 (as same as the setting)
+    initialLocUAV = [20.22, 20.23, 15.0]
 
     # initial movement
     initialMovement = [3, 1, 2, 5, 4, 6, 0]
@@ -50,3 +82,6 @@ if __name__ == '__main__':
     print('\noptimal path + locations of UAV with the optimal path :')
     for i in range(len(optimalPath)):
         print(i, optimalPath[i], locsUAV[i])
+
+    # save trajectory
+    saveTrajectory(width, height, initialLocUAV, locsUAV, deviceList)
