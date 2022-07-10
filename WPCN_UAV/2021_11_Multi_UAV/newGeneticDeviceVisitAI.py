e88a1ab9d368f6a4636b1760c2053e764e1d66c8
Hong Sik Kim <32893014+WannaBeSuperteur@users.noreply.github.com>
2022-07-10 14:04:41 +0900

    add: computing distance and writing data

--- WPCN_UAV/2021_11_Multi_UAV/newGeneticDeviceVisitAI.py
+++ WPCN_UAV/2021_11_Multi_UAV/newGeneticDeviceVisitAI.py
@@ -1,6 +1,10 @@
 import throughputTest_NewGenetic as T_NG
 import numpy as np
 import random
+import math
+
+def doBruteForce(deviceList, initialLocUAV, initialMovement):
+    pass
 
 def test(input_data, output_data):
     
@@ -22,8 +26,30 @@ def test(input_data, output_data):
         deviceList[i][1] = (deviceList[i][1] - avg[1]) / stddev[1]
 
     initialLocUAV = [0, 0, 0]
+    initialMovement = list(range(n))
 
     # test total distance
+    swappedMovement    = T_NG.swapBasic(deviceList, initialLocUAV, initialMovement, printed)
+    bruteForceMovement = doBruteForce(deviceList, initialLocUAV, initialMovement)
+
+    # compute total distance
+    totalDistSwapped    = T_NG.computeTotalDist(initialLocUAV, swappedMovement   , deviceList)
+    totalDistBruteForce = T_NG.computeTotalDist(initialLocUAV, bruteForceMovement, deviceList)
+
+    # write input and output data
+    input_d = []
+    
+    for i in range(n):
+        if i == 0:
+            input_data.append(deviceList[0][0])
+            input_data.append(deviceList[0][1])
+        else:
+            input_data.append(deviceList[i][0] - deviceList[i-1][0])
+            input_data.append(deviceList[i][1] - deviceList[i-1][1])
+
+    # append to input_data and output_data
+    input_data .append(input_d)
+    output_data.append([math.log(totalDistBruteForce / totalDistSwapped, 2.0)])
 
 if __name__ == '__main__':
 
